1bfc2514422f0bd0e6a8e17e21ad6a94bc50684a
Angry_Rabbit <yinbozhang.e@gmail.com>
2025-11-18 17:07:03 -0500

    add maven project

---
+++ dice/src/test/java/ybz/sprog/AppTest.java
@@ -0,0 +1,19 @@
+package ybz.sprog;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
